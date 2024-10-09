cb22fb50737632f35039309b61807beebf48b127
Levi <lnatamm25@gmail.com>
2024-10-09 19:41:55 -0300

    implementando classe MetodoGrafico e seu construtor

---
+++ main.py
@@ -0,0 +1,11 @@
+INF = 100000
+class MetodoGrafico:
+    def __init__(self, c1, c2, min):
+        self.c = [c1, c2]
+        self.min = min
+        self.Ab = [
+            [-1, 0, 0],
+            [1, 0, INF],
+            [0, -1, 0],
+            [0, 1, INF]
+        ]
