48d435fe619e4deac8622a96cee7d10247a7ed6f
Levi <lnatamm25@gmail.com>
2024-10-09 19:48:34 -0300

    implementando esqueleto dos métodos da classe

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ class MetodoGrafico:
             [0, -1, 0],
             [0, 1, INF]
         ]
+    
+    def AddEq(a1, b1, b): # Adiciona a equação 𝑎1𝑥 + 𝑎2𝑦 = 𝑏 à matriz 𝐴𝑏
+        pass
+
+    def ListEq(): # Lista as equações, numeradas de 0 a 𝑛 − 1
+        pass
+
+    def RemoveEq(k): # remove a 𝑘-ésima equação da matriz 𝐴𝑏. Não é possível remover as equações 0, 1, 2 e 3, dadas pela inicialização
+        pass
+    
+    def CheckPoint(x0, y0): # Testa se o ponto (𝑥0, 𝑦0) satisfaz todas as condições do problema
+        pass
+
+    def GetSolution(): # Retorna uma solução ótima do problema. Caso a solução tenha alguma de suas coordenadas valendo 𝐼𝑁𝐹, retorna a string "Função Ilimitada".
+        pass
